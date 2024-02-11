c0f762111cf3b0a63b4ae232a095528552352007
Micha Amir Cohen <micha@dataloop.ai>
2024-02-11 14:53:11 +0200

    task/CE-391/CE-392/aws-import-export-annotations

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# aws-hooks
-Import and Export annotations to AWS bucket
+# AWS Hooks
+
+The **AWS Hooks** application has two nodes to Export and Import annotation directly from AWS bucket.  \
+
+## Quick Start:
+
+1. Go to `Pipelines` and `Create new pipeline`.
+2. Build a custom work flow that requires Export/Import annotations to/from AWS bucket
+3. Define the bucket name and the region name in the node configuration panel. \
+4. Start pipeline
+
+Pre-requirements: The GPC-hooks service get integration_name parameter, the integration name is the secret name that holds the service account json data.  
+
+Also, secret with the following  format can be used as well (put the secrete name in the integration name filed)
+
+```
+{
+    "key": "your_access_key", 
+    "secret": "your_secret_key"
+}
+```
+
+## Node inputs and Outputs:
+
+Both aws-hooks2 nodes get the same item as input and output
+
+## How it works:
+
+### Export Annotations to AWS
+When an item passes through the node, will export the annotations to a json file and upload it to the AWS bucket. \
+The file will be uploaded to the following location: \
+`<driver_path>/<item.dir>/<item.name>.json`
+
+### Import Annotations from AWS
+When an item passes through the node, will download the JSON annotations file from the AWS bucket and update the item with the new annotations. \
+The file will be downloaded from the following location: \
+`<driver_path>/<item.dir>/<item.name>.json`
+
+
+## Node Configuration:
+
+<img src="assets/node_configration.png" height="480">
+
+**Configuration**
+
+- **Node Name:** Display name on the canvas.
+- **Bucket Name:** the bucket name to export/import the annotations
+- **Region Name:** the region name of the bucket
+
+
+## Contributions, Bugs and Issues - How to Contribute
+
+We welcome anyone to help us improve this app.  
+[Here's](CONTRIBUTING.md) a detailed instructions to help you open a bug or ask for a feature request.
---
+++ modules/aws_hooks.py
@@ -0,0 +1,104 @@
+import dtlpy as dl
+import logging
+import base64
+import boto3
+import json
+import os
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(name='AWS Export & Import')
+
+
+class HookIntegrationAWS(dl.BaseServiceRunner):
+    def __init__(self, integration_name):
+        aws_credentials = os.environ.get(integration_name.replace('-', '_'))
+        if aws_credentials is not None:
+            try:
+                # for case of secret
+                aws_credentials = json.loads(aws_credentials)
+            except json.JSONDecodeError:
+                # for case of integration
+                decoded_bytes = base64.b64decode(aws_credentials)
+                aws_credentials = decoded_bytes.decode("utf-8")
+                aws_credentials = json.loads(aws_credentials)
+                logger.info(f"integration_name = {integration_name}")
+        else:
+            # for local development
+            with open(os.path.join('..', 'aws-credentials.json')) as json_file:
+                aws_credentials = json.load(json_file)
+
+        self.aws_secret_access_key = aws_credentials['secret']
+        self.aws_access_key_id = aws_credentials['key']
+
+    def export_annotation(self, item: dl.Item, context: dl.Context):
+        if context is not None and context.node is not None and 'customNodeConfig' in context.node.metadata:
+            bucket_name = context.node.metadata['customNodeConfig']['bucket_name']
+            region_name = context.node.metadata['customNodeConfig']['region_name']
+            aws_rekognition_keys = {'region_name': region_name,
+                                    'aws_access_key_id': self.aws_access_key_id,
+                                    'aws_secret_access_key': self.aws_secret_access_key}
+
+            logger.info('Bucket name set to: {}'.format(bucket_name))
+        else:
+            raise ValueError("Node configration in context is missing, can't determinate the bucket name")
+
+        annotation_json = item.to_json()
+        annotation_json['annotations'] = item.annotations.list().to_json()['annotations']
+        filename, _ = os.path.splitext(item.filename)
+        s3 = boto3.client("s3", **aws_rekognition_keys)
+        filename = f"{filename[1:]}.json"
+        s3.put_object(Bucket=bucket_name, Key=filename, Body=json.dumps(annotation_json))
+        print(f"File {filename} uploaded to {bucket_name}/{filename}")
+
+        return item
+
+    def import_annotation(self, item: dl.Item, context: dl.Context):
+        if context is not None and context.node is not None and 'customNodeConfig' in context.node.metadata:
+            bucket_name = context.node.metadata['customNodeConfig']['bucket_name']
+            region_name = context.node.metadata['customNodeConfig']['region_name']
+            aws_rekognition_keys = {'region_name': region_name,
+                                    'aws_access_key_id': self.aws_access_key_id,
+                                    'aws_secret_access_key': self.aws_secret_access_key}
+
+            logger.info('Bucket name set to: {}'.format(bucket_name))
+        else:
+            raise ValueError("Node configration in context is missing, can't determinate the bucket name")
+
+        s3 = boto3.client("s3", **aws_rekognition_keys)
+        filename, _ = os.path.splitext(item.filename)
+        filename = f"{filename[1:]}.json"
+        response = s3.get_object(Bucket=bucket_name, Key=filename)
+        json_data = response['Body'].read().decode('utf-8')
+        data = json.loads(json_data)
+        item.annotations.upload(annotations=data['annotations'])
+        return item
+
+
+def test():
+    class Node:
+        def __init__(self, metadata):
+            self.metadata = metadata
+
+    dl.setenv('rc')
+    service_runner = HookIntegrationAWS(integration_name="")
+    original_item = dl.items.get(item_id='65c1160e06359112b9e8e980')
+    original_annotations = original_item.annotations.list()
+    remote_filepath = "/clones/1.jpg"
+    try:
+        item = original_item.dataset.items.get(filepath=remote_filepath)
+        item.delete()
+    except dl.exceptions.NotFound:
+        pass
+
+    item = original_item.clone(remote_filepath=remote_filepath)
+    context = dl.Context()
+    context._node = Node(metadata={'customNodeConfig': {'bucket_name': 'micha-test', 'region_name': 'us-east-1'}})
+    service_runner.export_annotation(item=item, context=context)
+    item.annotations.delete(filters=dl.Filters(resource=dl.FiltersResource.ANNOTATION))
+    service_runner.import_annotation(item=item, context=context)
+    assert len(item.annotations.list()) == len(original_annotations)
+
+
+if __name__ == '__main__':
+    dl.setenv('rc')
+    test()
